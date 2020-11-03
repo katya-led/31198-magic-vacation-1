6e735a49fd27bad5fec74c8fc754f09d5c6eac5c
katya-led <ekaterina.ledneva@bquadro.ru>
2020-11-03 13:39:44 +0300

    1-2

---
+++ source/js/modules/body.js
@@ -0,0 +1,3 @@
+export default () => {
+    document.body.onload = () => document.body.classList.add(`active`);
+};
--- source/js/script.js
+++ source/js/script.js
@@ -1,4 +1,5 @@
 // modules
+import body from "./modules/body.js";
 import mobileHeight from './modules/mobile-height-adjust.js';
 import slider from './modules/slider.js';
 import menu from './modules/menu.js';
@@ -10,6 +11,7 @@ import social from './modules/social.js';
 import FullPageScroll from './modules/full-page-scroll';
 
 // init modules
+body();
 mobileHeight();
 slider();
 menu();
